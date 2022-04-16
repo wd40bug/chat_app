715a883a5a73b9b5383b27482f38e0019e0deb5d
wd40bug <wd40bug@gmail.com>
2022-04-16 16:07:25 -0600

    basic echo server implemented

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,29 +1,49 @@
-use std::{
-    io::{self, BufRead},
-    net::{TcpListener, ToSocketAddrs},
+use std::net::{TcpListener, ToSocketAddrs};
+
+use async_std::{
+    io::{prelude::BufReadExt, BufReader, WriteExt},
+    net::TcpStream,
+    prelude::StreamExt,
+    task::{self, JoinHandle},
 };
+use futures::Future;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+fn main() {
+    accept_loop("127.0.0.1:8080").unwrap();
+}
 
 fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
     let listener = TcpListener::bind(addr)?;
     let mut incoming = listener.incoming();
-    while let Some(Ok(mut result)) = incoming.next() {
-        let mut reader = io::BufReader::new(&mut result);
-        let recieved: Vec<u8> = reader.fill_buf()?.to_vec();
-        reader.consume(recieved.len());
-        String::from_utf8(recieved)
-            .map(|msg| println!("{}", msg))
-            .map_err(|_| {
-                io::Error::new(
-                    io::ErrorKind::InvalidData,
-                    "Couldn't parse received string as utf8",
-                )
-            })?
+    while let Some(Ok(result)) = incoming.next() {
+        let _handle = spawn_and_log_error(handle_client(TcpStream::from(result)));
     }
     Err("incoming.next returned a None(documentation says is impossible)")?
 }
 
-fn main() {
-    accept_loop("127.0.0.1::8080").unwrap();
+async fn handle_client(stream: TcpStream) -> Result<()> {
+    let mut writer = &stream;
+    let reader = BufReader::new(&stream);
+    let mut lines = reader.lines();
+    println!("accepted {}", stream.peer_addr()?);
+    while let Some(line) = lines.next().await {
+        let line = line?;
+        println!("{}", &line);
+        writer.write_all(line.as_bytes()).await?;
+        writer.write_all(b"\n").await?;
+    }
+    Ok(())
+}
+
+fn spawn_and_log_error<F>(fut: F) -> JoinHandle<()>
+where
+    F: Future<Output = Result<()>> + Send + 'static,
+{
+    task::spawn(async move {
+        if let Err(e) = fut.await {
+            eprintln!("{}", e);
+        }
+    })
 }
