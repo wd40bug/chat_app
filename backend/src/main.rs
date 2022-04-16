e8a969531540d9e8d92675558aa77f949a51f710
wd40bug <wd40bug@gmail.com>
2022-04-16 10:37:46 -0600

    basic server for testing may work

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,25 @@
-use async_std::{
-    // 3
-    prelude::*, // 1
-    task,       // 2
+use std::{
+    io::{self, BufRead},
+    net::{TcpListener, ToSocketAddrs},
 };
-use std::net::{TcpListener, ToSocketAddrs};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
     let listener = TcpListener::bind(addr)?;
     let mut incoming = listener.incoming();
-    while let Some(result) = incoming.next() {
-        let stream = result?;
+    while let Some(Ok(mut result)) = incoming.next() {
+        let mut reader = io::BufReader::new(&mut result);
+        let recieved: Vec<u8> = reader.fill_buf()?.to_vec();
+        reader.consume(recieved.len());
+        String::from_utf8(recieved)
+            .map(|msg| println!("{}", msg))
+            .map_err(|_| {
+                io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    "Couldn't parse received string as utf8",
+                )
+            })?
     }
     Err("incoming.next returned a None(documentation says is impossible)")?
 }
